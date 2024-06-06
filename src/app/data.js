b809ec83aa78770b4d8a53a8ad859fbf174f5700
luuizz <luu.izz@hotmail.com>
2024-06-05 22:12:30 -0300

    feat: criado arquivo de dados

---
+++ src/app/data.js
@@ -0,0 +1,49 @@
+import logoGoogle from "@/assets/logo-google.svg"
+import logoFacebook from "@/assets/logo-facebook.svg"
+import logoInstagram from "@/assets/logo-instagram.svg"
+import iconAlcanse from "@/assets/icon-alcance.svg"
+import iconGaste from "@/assets/icon-gaste.svg"
+import iconAutonomia from "@/assets/icon-autonomia.svg"
+import iconCloudboost from "@/assets/icon-cloudboost.svg"
+
+export const clientsLogo = [
+    {
+        image: logoGoogle,
+        alt: 'Logo do Google',
+    },
+    {
+        image: logoFacebook,
+        alt: 'Logo do Facebook',
+    },
+    {
+        image: logoInstagram,
+        alt: 'Logo do Instagram',
+    },
+]
+
+export const dataRecursos = [
+    {
+        icon: iconAlcanse,
+        altIcon: 'Ícone de alcance mais',
+        title: 'Alcance mais',
+        text: 'Aumente o tráfego do seu site e suas vendas utilizando o e-mail marketing.',
+    },
+    {
+        icon: iconGaste,
+        altIcon: 'Gaste menos',
+        title: 'Gaste menos',
+        text: 'Com a ferramenta você faz envios em massa, gastando menos de R$ 0,01 por mensagem!',
+    },
+    {
+        icon: iconAutonomia,
+        altIcon: 'Autonomia e resultado',
+        title: 'Autonomia e resultado',
+        text: 'Crie listas de contatos, campanhas e ainda analise o resultado de cada envio.',
+    },
+    {
+        icon: iconCloudboost,
+        altIcon: 'Ícone do Cloudboost',
+        title: 'Template Boost',
+        text: 'Modelos prontos para cada tipo de e-mail: newsletter, lançamento, campanha de vendas e outros.',
+    },
+]
