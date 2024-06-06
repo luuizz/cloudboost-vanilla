cf11e3b1b2f2eecfe2a5a8ca062c7d1418dc6b29
luuizz <luu.izz@hotmail.com>
2024-06-05 22:12:44 -0300

    configurado fontes padrões

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,23 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Inter, Space_Grotesk } from "next/font/google";
+import './app.css'
+import './globals.css'
+import Header from "@/components/Header";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ 
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  display: "swap",
+  adjustFontFallback:  false,
+  variable: "--font-texts",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["600", "700"],
+  display: "swap",
+  adjustFontFallback:  false,
+  variable: "--font-title",
+})
 
 export const metadata = {
   title: "Cloudboost | Ferramenta de E-mail Marketing",
@@ -11,7 +27,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
